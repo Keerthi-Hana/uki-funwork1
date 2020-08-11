f2c0a6b580ef187386392e370a2fe2624c427248
Keerthi-hana <keerthikiki20@gmail.com>
2020-08-11 12:41:19 +0530

    Uki Funwork1 Files

---
+++ main.js
@@ -0,0 +1,22 @@
+SC.initialize({
+    client_id: '340f063c670272fac27cfa67bffcafc4'
+  });
+  $(document).ready(function() { 
+      //stream the track number song
+    SC.stream('/tracks/29545087',function(sound){
+   
+        //function for start button
+        $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+     
+          //function for stop button
+          $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+     });
+
+   }); 
